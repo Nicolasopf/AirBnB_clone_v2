f3c7331461e7764cf6bf80261f22ac676d957f7f
Nicolas <n.ico15935746@gmail.com>
2021-04-15 02:23:51 -0500

    [Updated] No statement needed in rm; semicolon added for sed command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,15 +5,11 @@ apt-get install nginx -y
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared
 link="/data/web_static/current"
-
-if [ -L $link ]; then
-	rm -rf "$link"
-fi
-
-ln -s /data/web_static/releases/test/ "$link"
+rm -rf "$link"
+ln -sf /data/web_static/releases/test/ "$link"
 echo "Holberton School" > /data/web_static/releases/test/index.html
 chown -R ubuntu:ubuntu /data/
 if [[ $(grep -c "alias /data/web_static/current" /etc/nginx/sites-available/default) -ge 1 ]]; then
-	sed -i '/server_name _;/a\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/\n\t}' /etc/nginx/sites-available/default
+	sed -i '/server_name _;/a\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 fi
 service nginx restart
