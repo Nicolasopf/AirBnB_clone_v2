7f15e34f2c66d65c4a5284cbb59a359848bbf1ce
Nicolas <n.ico15935746@gmail.com>
2021-04-15 14:52:01 -0500

    [Updated] Change almost every line

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
-# Setup installation, creating folders, install nginx, give ownerships and create symbolic links
+# sets up your web servers for the deployment of web_static.
 apt-get update
 apt-get install nginx -y
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-link="/data/web_static/current"
-rm -rf "$link"
-ln -s /data/web_static/releases/test/ "$link"
-echo "Holberton School" > /data/web_static/releases/test/index.html
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
+if [ ! -f "/data/web_static/releases/test/index.html" ]; then
+    echo "Mock Page" > /data/web_static/releases/test/index.html
+fi
+rm -rf /data/web_static/current
+ln -s /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
 str="\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
 if [ "$(grep -c "location /hbnb_static {" /etc/nginx/sites-available/default)" -eq 0 ]; then
     sed -i "45i $str" /etc/nginx/sites-available/default
