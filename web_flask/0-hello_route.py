c4abf4c566fdb214f1fa3a2d91db0d0d8a84eb6c
Nicolas <n.ico15935746@gmail.com>
2021-05-01 15:05:41 -0500

    [Added] Comments

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+''' Turn on flask web app with multiple routes '''
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -6,31 +7,37 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def index():
+    ''' First page to load when entering 0:5000 '''
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
+    '''' hbnb path show HBNB '''
     return "HBNB"
 
 
 @app.route('/c/<text>')
 def c_is(text):
+    ''' text: used to print C + /text '''
     return 'C ' + text.replace("_", " ")
 
 
 @app.route('/python')
 def python_alone():
+    ''' route python to show python is cool '''
     return "Python is cool"
 
 
 @app.route('/python/<text>')
-def python(text="is cool"):
+def python(text):
+    ''' route python to show the text user passed as subpath '''
     return "Python " + text.replace("_", " ")
 
 
 @app.route('/number/<int:n>')
 def number(n):
+    ''' Prints <number> is a number if is a number.'''
     return "{} is a number".format(n)
 
 
