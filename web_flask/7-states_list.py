235f6e631f195507f7e4d520d081a62f3597b390
Nicolas <n.ico15935746@gmail.com>
2021-05-02 18:27:50 -0500

    [Updated] Strict slashes property place

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,10 +5,9 @@ from flask import render_template
 from models.state import State
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/states_list')
+@app.route('/states_list', strict_slashes=False)
 def index():
     """ Index to display lists """
     states = storage.all(State)
